8e51836d0ae5bea5e9813d3d1ee9ac144a1e7aa0
John Dougan <void.random@gmail.com>
2015-06-19 13:00:10 -0700

    work on password support

--- starttest2.sh
+++ starttest2.sh
@@ -4,5 +4,5 @@ echo -n 'XRFBPASSWD=' > ${XRFBSESSIONID}/xrfbpasswd
 echo 'tea41dragon' >> ${XRFBSESSIONID}/xrfbpasswd
 export XRFBHOSTPORT=5901
 docker run -p ${XRFBHOSTPORT}:5900 -e XRFBRESX=1000 -e XRFBRESY=1000 -e XRFBDEPTH=24 --env-file xrfbpasswd \
-    --cidfile="${XRFBSESSIONID}/containerid"  --label=net.opencobalt.xrfb.session=${XRFBSESSIONID} \
+    --cidfile=${XRFBSESSIONID}/containerid  --label=net.opencobalt.xrfb.session=${XRFBSESSIONID} \
     jdougan/base-xrfb-chrome:1
